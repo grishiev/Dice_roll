a3e13a78ebb78d968c1d98e17b8af7ef4d809e13
grishiev <grishiev@amazon.com>
2023-04-27 11:52:31 +0100

    Initial commit

---
+++ dice.py
@@ -0,0 +1,14 @@
+import random
+
+dice1 = 0
+dice2 = 0
+
+while True:
+	answer = input("Would you likke to through the dice? Y for 'yes', N for 'no': ")
+	if answer == "y" or answer == "Y":
+		dice1 = random.randrange(1, 7, 1)
+		dice2 = random.randrange(1, 7, 1)
+		print("You rolled ", dice1, " and ", dice2)
+	if answer == "n" or answer == "N":
+		print("Thank you! See you some other time.")		
+		break
